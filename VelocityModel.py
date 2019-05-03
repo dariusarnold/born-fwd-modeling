1a51728d8ae069185873a0de4ff60ffde27ec399
Darius Arnold <mail@dariusarnold.de>
2019-05-03 19:52:23 +0200

    Add __repr__ to Vector3D to show values in debugger

--- VelocityModel.py
+++ VelocityModel.py
@@ -36,6 +36,9 @@ class Vector3D:
     def __getitem__(self, item: int) -> float:
         return self.data[item]
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.x}, {self.y}, {self.z})"
+
 
 class VelocityModel:
     """
