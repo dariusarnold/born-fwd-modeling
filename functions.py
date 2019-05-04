12747ce07443d7f2378ae60c4f8ab3e9e3ddef76
Darius Arnold <mail@dariusarnold.de>
2019-05-04 10:36:02 +0200

    Pass arguments during integration instead of using partial functions

--- functions.py
+++ functions.py
@@ -55,22 +55,32 @@ def greens_function(density: float, v0: float, x: Vector3D, x_prime: Vector3D,
     return a * b
 
 
-def born_modeling(xs: Vector3D, xr: Vector3D, omega: float, w_central: float, density: float,
+def integral(z: float, y: float, x:float, additional_params:Tuple):
+    x_prime = Vector3D(x, y, z)
+    density, v0, omega, velocity_model, xs, xr = additional_params
+    v = velocity_model.eval_at(x_prime)
+    epsilon = scattering_potential(v, v0)
+    # since the product contains a zero dont compute other functions to save operations
+    if epsilon == 0:
+        return 0.
+    G0_left = greens_function(density, v0, xs, x_prime, omega)
+    G0_right = greens_function(density, v0, x_prime, xr, omega)
+    return G0_left * epsilon * G0_right
+
+
+def born_modeling(xs: Vector3D, xr: Vector3D, omega: float, omega_central: float, density: float,
                   velocity_model: VelocityModel) -> complex:
     """
 
     :param xs:
     :param xr:
     :param omega:
-    :param w_central:
+    :param omega_central:
     :param density:
     :param velocity_model:
     :return:
     """
     v0 = velocity_model.bg_vel
-    W = functools.partial(ricker_frequency_domain, omega_central=w_central)
-    G0 = functools.partial(greens_function, density, v0)
-    epsilon = functools.partial(scattering_potential, v0=v0)
 
     x_start = 0.
     x_end = velocity_model.x_width
@@ -79,25 +89,23 @@ def born_modeling(xs: Vector3D, xr: Vector3D, omega: float, w_central: float, de
     z_start = 0
     z_end = velocity_model.scatterer_max_depth
 
-    def integral(z, y, x):
-        x_prime = Vector3D(x, y, z)
-        return G0(xs, x_prime, omega) * epsilon(velocity_model.eval_at(x_prime)) * G0(x_prime, xr, omega)
-
     # scipy cant integrate complex functions, so we split integration into real
     # and imaginary part
-    def real_func(z, y, x):
-        return scipy.real(integral(z, y, x))
+    def real_func(z, y, x, *args):
+        return scipy.real(integral(z, y, x, args))
 
-    def imag_func(z, y, x):
-        return scipy.imag(integral(z, y, x))
+    def imag_func(z, y, x, *args):
+        return scipy.imag(integral(z, y, x, args))
 
     y_real, abserr_real = integrate.tplquad(real_func, x_start, x_end,
-                                  y_start, y_end,
-                                  z_start, z_end)
+                                            y_start, y_end,
+                                            z_start, z_end,
+                                            args=(density, v0, omega, velocity_model, xs, xr))
     y_imag, abserr_imag = integrate.tplquad(imag_func, x_start, x_end,
                                             y_start, y_end,
-                                            z_start, z_end)
+                                            z_start, z_end,
+                                            args=(density, v0, omega, velocity_model, xs, xr))
 
-    y_real *= W(omega) * omega ** 2
-    y_imag *= W(omega) * omega ** 2
+    y_real *= ricker_frequency_domain(omega, omega_central) * omega**2
+    y_imag *= ricker_frequency_domain(omega, omega_central) * omega**2
     return complex(y_real, y_imag)
