ac47a5a9567d4ecc90b33991d2a0ef8ce2e156fc
Darius Arnold <mail@dariusarnold.de>
2019-05-06 12:35:12 +0200

    Change integration to use nquad, pass smaller error bounds, add comments

--- functions.py
+++ functions.py
@@ -3,7 +3,6 @@ import math
 from typing import Tuple
 
 import fastfunctions
-import scipy
 from scipy import integrate
 
 from VelocityModel import Vector3D, VelocityModel
@@ -31,9 +30,7 @@ def scattering_potential(v: float, v0: float) -> float:
 
 
 def length(vector: Vector3D) -> float:
-    """
-    Calculate length of vector
-    """
+    """Calculate length of vector"""
     return math.sqrt(sum(component**2 for component in vector))
 
 
@@ -49,61 +46,87 @@ def greens_function(density: float, v0: float, x: Vector3D, x_prime: Vector3D,
     :param omega: Frequency
     :return:
     """
-    # split formula for better readability
+    # pull out length from formual to calculate it only once
     l = length(x - x_prime)
+    # split formula for better readability
     a = 1. / (4. * math.pi * density * v0**2 * l)
     b = cmath.exp(-1j * omega * l / v0)
     return a * b
 
 
-def integral(z: float, y: float, x:float, additional_params:Tuple):
+def integral(x: float, y: float, z: float, additional_params: Tuple):
+    """
+    Triple integral from Hu2018a eq. 1.
+    :param x: x coordinate of x_prime
+    :param y: y coordinate of x_prime
+    :param z: z_coordinate of x_prime
+    :param additional_params:
+    :return:
+    """
     x_prime = Vector3D(x, y, z)
     density, v0, omega, velocity_model, xs, xr = additional_params
     v = velocity_model.eval_at(x_prime)
-    # could check if v == background_vel and return 0 if true
-    epsilon = fastfunctions.scattering_potential(v, v0)
-    # since the product contains a zero dont compute other functions to save operations
-    if epsilon == 0:
+    if v == v0:
+        # early exit to save function evaluations in inner loop
+        # scattering potential epsilon is 0 when v = v0 and the full integral term
+        # is a product with that 0
         return 0.
+    epsilon = fastfunctions.scattering_potential(v, v0)
     G0_left = fastfunctions.greens_function(density, v0, xs, x_prime, omega)
     G0_right = fastfunctions.greens_function(density, v0, x_prime, xr, omega)
     return G0_left * epsilon * G0_right
 
 
 def born_modeling(xs: Vector3D, xr: Vector3D, omega: float, omega_central: float, density: float,
-                  velocity_model: VelocityModel) -> complex:
+                  velocity_model: VelocityModel, epsabs: float = 1E-16, epsrel: float = 1E-16,
+                  limit: int = 50) -> complex:
     """
-
-    :param xs:
-    :param xr:
-    :param omega:
-    :param omega_central:
-    :param density:
-    :param velocity_model:
+    Calculate scattered P wave in the frequency domain (eq. 1 from Hu2018a) using
+    born approximation. Evaluate for a frequency range to get the P wave spectrum.
+    Source wavelet is a Ricker wavelet.
+    :param xs: Source position
+    :param xr: Receiver position
+    :param omega: Frequency for which the P wave spectrum should be calculated.
+    :param omega_central: Central frequency of Ricker source wavelet
+    :param density: Density in kg/m^3 of Model
+    :param velocity_model: Used to evaluate velocity at integration point
+    :param epsabs: Absolute error tolerance of integration, passed on to scipy.integrate.nquad
+    :param epsrel: Relative error tolerance of integration, passed on to scipy.integrate.nquad
+    :param limit: An upper bound on the number of subintervals used in the adaptive algorithm,
+    passed on to scipy.integrate.nquad
     :return:
     """
     v0 = velocity_model.bg_vel
 
+    # integration limits. Since integral is zero everywhere outside of the scatterers,
+    # we can limit the integration area
     x_start = 0.
     x_end = velocity_model.x_width
     y_start = 0.
     y_end = velocity_model.y_width
     z_start = velocity_model.scatterer_top
     z_end = velocity_model.scatterer_bottom
+    integration_limits = [(x_start, x_end), (y_start, y_end), (z_start, z_end)]
 
     # scipy cant integrate complex functions, so we split integration into real
     # and imaginary part
-    def real_func(z, y, x, *args):
-        return scipy.real(integral(z, y, x, args))
+    def real_func(x, y, z, *args):
+        return integral(x, y, z, args).real
 
-    def imag_func(z, y, x, *args):
-        return scipy.imag(integral(z, y, x, args))
+    def imag_func(x, y, z, *args):
+        return integral(x, y, z, args).imag
 
-    y_real, abserr_real = integrate.tplquad(real_func, x_start, x_end, y_start, y_end, z_start, z_end,
-                                            args=(density, v0, omega, velocity_model, xs, xr))
-    y_imag, abserr_imag = integrate.tplquad(imag_func, x_start, x_end, y_start, y_end, z_start, z_end,
-                                            args=(density, v0, omega, velocity_model, xs, xr))
+    # additional arguments for integration that are passed to the integral function
+    args = (density, v0, omega, velocity_model, xs, xr)
 
+    # integration options
+    opts = [{"epsabs": epsabs, "epsrel": epsrel, "limit": limit} for _ in range(3)]
+
+    # Calculate triple integral
+    y_real, *_ = integrate.nquad(real_func, integration_limits, args=args, opts=opts)
+    y_imag, *_ = integrate.nquad(imag_func, integration_limits, args=args, opts=opts)
+
+    # Add factor
     factor = fastfunctions.ricker_frequency_domain(omega, omega_central) * omega**2
     y_real *= factor
     y_imag *= factor
