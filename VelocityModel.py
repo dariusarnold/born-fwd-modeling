97fc2cb0d36b3304670cedfe40ca553bcdf4d24d
Darius Arnold <mail@dariusarnold.de>
2019-05-29 12:32:18 +0200

    Add argparse to run main script from cmd

--- VelocityModel.py
+++ VelocityModel.py
@@ -1,3 +1,6 @@
+import abc
+import itertools
+
 import numpy as np
 from typing import Tuple, Sequence
 
@@ -44,16 +47,65 @@ class Vector3D:
         return Vector3D(self.x-other.x, self.y-other.y, self.z-other.z)
 
 
-class VelocityModel:
+class AbstractVelocityModel(abc.ABC):
+    """
+    Interface expected from a class that can replace the default velocity model
+    """
+    @abc.abstractmethod
+    def eval_at(self, position: Tuple[float, float, float]) -> float:
+        """Return velocity (m/s) at coordinate x y z (m)"""
+        pass
+
+    # These attributes need to be present
+    @property
+    @abc.abstractmethod
+    def density(self) -> float:
+        """Return homogeneous density in kg/m^3"""
+        pass
+
+    @property
+    @abc.abstractmethod
+    def x_width(self) -> float:
+        """Return width of model along x axis in m"""
+        pass
+
+    @property
+    @abc.abstractmethod
+    def y_width(self) -> float:
+        """Return width of model along y axis in m"""
+        pass
+
+    @property
+    @abc.abstractmethod
+    def scatterer_top(self) -> float:
+        """Return depth of uppermost scatterer in m (with the radius) """
+        pass
+
+    @property
+    @abc.abstractmethod
+    def scatterer_bottom(self) -> float:
+        """Return depth of bottom most scatterer in m(with the radius)"""
+        pass
+
+
+
+class VelocityModel(AbstractVelocityModel):
     """
     Velocity model with a homogeneous background velocity that represents fractures
     as a collection of close scatterer points with a circular velocity heterogenity
     around them.
     """
+    # Attributes from the ABC need to be listed here if they need to be initialised in the
+    # init method
+    density = None
+    x_width = None
+    y_width = None
+    scatterer_top = None
+    scatterer_bottom = None
 
-    def __init__(self, background_velocity: float, fracture_velocity: float,
-                 x_width: float, y_width: float,
-                 scatterer_positions: Sequence[Vector3D], scatterer_radius: float):
+    def __init__(self, background_velocity: float, fracture_velocity: float, x_width: float,
+                 y_width: float, scatterer_positions: Sequence[Vector3D], scatterer_radius: float,
+                 density: float):
         """
         Create new model
         :param background_velocity: unit m/s
@@ -63,11 +115,13 @@ class VelocityModel:
         :param scatterer_positions: All scatterer points in the model. A scatterer is a point in the
         model around which there is a velocity anomaly
         :param scatterer_radius: Radius of the velocity anomaly around a scatterer, unit m
+        :param density: Homogeneous background density of model in kg/m^3
         """
         self.bg_vel = background_velocity
         self.frac_vel = fracture_velocity
         self.x_width = x_width
         self.y_width = y_width
+        self.density = density
         self.scatterer_tree = scipy.spatial.cKDTree(np.asarray(scatterer_positions))
         self.scatterer_positions = scatterer_positions
         self.scatterer_radius = scatterer_radius
@@ -88,3 +142,45 @@ class VelocityModel:
             return self.frac_vel
         else:
             return self.bg_vel
+
+
+def create_scatterers() -> Sequence[Vector3D]:
+    """
+    Create a list of scatterer positions for the model described in 3D seismic characterization of
+    fractures in a dipping layer using the double-beam method (Hao Hu and Yingcai Zheng)
+    :return:
+    """
+    model_width = 11200.
+    # values taken from Fig4 in Hu2018a, all units m
+    frac_depth = 2400.
+    # fractures are in a square with sides parallel to the axes
+    # values taken from Fig 4 Hu2018a
+    frac_top_left_x = 600.
+    frac_top_left_y = model_width - 1300
+    frac_bot_right_x = model_width - 1400
+    frac_bot_right_y = 860.
+    frac_spacing = 200.
+    # distance between scatterer points, calculated as 1/8 p wave length
+    scatterer_spacing = 20.
+    # + 1 to cover the "last" point
+    number_of_fractures = int((frac_top_left_y - frac_bot_right_y) / frac_spacing) + 1
+    scatterers_per_fracture = int((frac_bot_right_x - frac_top_left_x) / scatterer_spacing) + 1
+    # step parallel to y axis
+    scatterer_y_positions = np.linspace(frac_bot_right_y, frac_top_left_y, number_of_fractures)
+    # step parallel to x axis
+    scatterer_x_positions = np.linspace(frac_top_left_x, frac_bot_right_x, scatterers_per_fracture)
+    xy_combinations = itertools.product(scatterer_x_positions, scatterer_y_positions)
+    scatterers = [Vector3D(x, y, frac_depth) for x, y in xy_combinations]
+    return scatterers
+
+
+def create_velocity_model() -> VelocityModel:
+    background_vel = 4800.
+    fracture_vel = 4320.
+    x_width = 11200.
+    y_width = 11200.
+    scatterer_positions = create_scatterers()
+    scatterer_radius = 100.
+    density = 2550.
+    return VelocityModel(background_vel, fracture_vel, x_width, y_width, scatterer_positions,
+                         scatterer_radius, density)
--- main.py
+++ main.py
@@ -1,49 +1,14 @@
-import itertools
+import argparse
+import importlib
 import math
 from concurrent.futures import ProcessPoolExecutor, as_completed
-from pprint import pprint
-from tqdm import tqdm
 
 import matplotlib.pyplot as plt
 import numpy as np
+from tqdm import tqdm
 
-from VelocityModel import VelocityModel, Vector3D
+from VelocityModel import VelocityModel, AbstractVelocityModel
 from functions import born_modeling
-
-
-def create_scatterers():
-    model_width = 11200.
-    # values taken from Fig4 in Hu2018a, all units m
-    frac_depth = 2400.
-    # fractures are in a square with sides parallel to the axes
-    # values taken from Fig 4 Hu2018a
-    frac_top_left_x = 600.
-    frac_top_left_y = model_width - 1300
-    frac_bot_right_x = model_width - 1400
-    frac_bot_right_y = 860.
-    frac_spacing = 200.
-    # distance between scatterer points, calculated as 1/8 p wave length
-    scatterer_spacing = 20.
-    # + 1 to cover the "last" point
-    number_of_fractures = int((frac_top_left_y - frac_bot_right_y) / frac_spacing) + 1
-    scatterers_per_fracture = int((frac_bot_right_x - frac_top_left_x) / scatterer_spacing) + 1
-    # step parallel to y axis
-    scatterer_y_positions = np.linspace(frac_bot_right_y, frac_top_left_y, number_of_fractures)
-    # step parallel to x axis
-    scatterer_x_positions = np.linspace(frac_top_left_x, frac_bot_right_x, scatterers_per_fracture)
-    xy_combinations = itertools.product(scatterer_x_positions, scatterer_y_positions)
-    scatterers = [Vector3D(x, y, frac_depth) for x, y in xy_combinations]
-    return scatterers
-
-
-def create_velocity_model():
-    background_vel = 4800.
-    fracture_vel = 4320.
-    x_width = 11200
-    y_width = 11200
-    scatterer_positions = create_scatterers()
-    scatterer_radius = 100
-    return VelocityModel(background_vel, fracture_vel, x_width, y_width, scatterer_positions, scatterer_radius)
 
 
 def plot_fractures(velocity_model: VelocityModel):
@@ -65,35 +30,88 @@ def angular(f):
 
 
 def main():
-    model = create_velocity_model()
 
-    # parameters
-    source_pos = Vector3D(model.x_width, model.y_width/2, 10.)
-    receiver_pos = Vector3D(5272., 3090., 0.)
+    def velocity_model(fname: str) -> VelocityModel:
+        """Create VelocityModel from given VelocityModel file name.
+        This function imports the file as a module and looks for a create_velocity_model function.
+        If found it is called and checked if the return value is a subclass of the AbstractVelocityModel.
+        The created velocity model is returned."""
+        try:
+            # try to import module from file
+            vm_module = importlib.import_module(fname.rstrip(".py"))
+        except ModuleNotFoundError:
+            raise argparse.ArgumentTypeError(f"The file {fname} does not exist!")
+        try:
+            # try to create velocity model
+            vm = vm_module.create_velocity_model()
+        except TypeError as e:
+            msg = ("Cant instantiate VelocityModel since the provided class doesn't implement all abstract functions. "
+                   f"Error encountered during instantiation: {e}")
+            raise argparse.ArgumentTypeError(msg)
+        except AttributeError:
+            msg = f"The provided module {fname} does not contain a create_velocity_model function."
+            raise argparse.ArgumentTypeError(msg)
+        if not issubclass(type(vm), AbstractVelocityModel):
+            msg = ("The VelocityModel is not registered as a subclass of the AbstractVelocityModel. Derive the velocity"
+                   " model class from AbstractVelocityModel")
+            raise argparse.ArgumentTypeError(msg)
+        return vm
+
+    parser = argparse.ArgumentParser(description="Use born modelling to generate seismic recordings from fracture \
+                                     scattered waves.",
+                                     fromfile_prefix_chars="@")
+    # Even though making optional arguments required is against command line conventions,
+    # this is the only way to use metavars to specify order of coordinates
+    # see https://bugs.python.org/issue14074
+    parser.add_argument("-s", "--source_pos", nargs=3, type=float, metavar=("xs", "ys", "zs"),
+                        required=True, help="coordinates of source (shot position) in m")
+    parser.add_argument("-r", "--receiver_pos", nargs=3, type=float, metavar=("xr", "yr", "zr"),
+                        required=True, help="coordinates of receiver (geophone position) in m")
+    parser.add_argument("output_filename", type=argparse.FileType("w"), help="Filename in which results will be saved")
+    parser.add_argument("-w", "--omega_central", type=lambda x: angular(float(x)), default=30., help="Central frequency of Ricker source\
+                        wavelet in Hz", metavar="Hz")
+    parser.add_argument("-n", "--num_of_frequency_steps", type=int, default=16, help="Number of evenly spaced \
+                        frequency samples to take between fmin and fmax")
+    parser.add_argument("--fmin", type=float, default=1., help="Minimal frequency for which u_scattering is calculated")
+    parser.add_argument("--fmax", type=float, default=100., help="Maximum frequency for which u_scattering is \
+                        calculated")
+    parser.add_argument("--serial", action="store_const", dest="processing", const="serial", default="parallel",
+                        help="This flag activates serial processing of the frequency samples instead of parallel (default)")
+    parser.add_argument("-c", "--cores", type=int, help="Number of cores for multiprocessing. If not specified, all \
+                        available cores will be used")
+    parser.add_argument("-v", "--velocity_model", type=velocity_model,
+                        default="VelocityModel.py", help="Specify file \
+                        from which the VelocityModel is created. The file should contain a create_velocity_model \
+                        function that returns a VelocityModel object, which has an eval_at method that takes a \
+                        (x, y, z) sequence of coordinates and returns the velocity at that point. An abstract base \
+                        class that has to be overridden is provided in VelocityModel.py")
+
+    args = parser.parse_args()
+    print(args)
+
     # angular frequencies in rad/s
-    omega_central = angular(30)
-    num_of_frequency_steps = 16
-    frequency_min = angular(1)
-    frequency_max = angular(100)
-    processing = "parallel"
-    # Move density into velocity model
-    density = 2550.  # kg/m^3
+    frequency_min = angular(args.fmin)
+    frequency_max = angular(args.fmax)
+    frequency_samples = np.linspace(frequency_min, frequency_max, args.num_of_frequency_steps)
+    born(args.source_pos, args.receiver_pos, args.velocity_model,
+         args.omega_central, frequency_samples, args.processing, args.cores)
 
-    frequency_samples = np.linspace(frequency_min, frequency_max, num_of_frequency_steps)
+
+def born(source_pos, receiver_pos, velocity_model, omega_central, frequency_samples, processing, num_cores):
     p_wave_spectrum = []
     futures = []
     fut_freq_mapping = {}
     if processing == "serial":
-        for frequency in tqdm(frequency_samples, desc="Born modeling", total=num_of_frequency_steps, unit="frequency samples"):
-            res = born_modeling(source_pos, receiver_pos, frequency, omega_central, density=density, velocity_model=model)
+        for frequency in tqdm(frequency_samples, desc="Born modeling", total=len(frequency_samples), unit="frequency samples"):
+            res = born_modeling(source_pos, receiver_pos, frequency, omega_central, density=density, velocity_model=velocity_model)
             p_wave_spectrum.append((frequency, res))
     elif processing == "parallel":
-        with ProcessPoolExecutor() as process_pool:
+        with ProcessPoolExecutor(num_cores) as process_pool:
             for frequency in frequency_samples:
-                future = process_pool.submit(born_modeling, source_pos, receiver_pos, frequency, omega_central, density=density, velocity_model=model)
+                future = process_pool.submit(born_modeling, source_pos, receiver_pos, frequency, omega_central, density=density, velocity_model=velocity_model)
                 futures.append(future)
                 fut_freq_mapping[future] = frequency
-            for future in tqdm(as_completed(futures), desc="Born modeling", total=num_of_frequency_steps, unit="frequency samples"):
+            for future in tqdm(as_completed(futures), desc="Born modeling", total=len(frequency_samples), unit="frequency samples"):
                 res = future.result()
                 frequency = fut_freq_mapping[future]
                 p_wave_spectrum.append((frequency, res))
