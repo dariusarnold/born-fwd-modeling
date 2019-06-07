c58d6af42101c1cf9ed688011bfcfee80476a2fa
Darius Arnold <mail@dariusarnold.de>
2019-06-07 20:30:46 +0200

    Formatting changes and a type hints in test.py

--- test.py
+++ test.py
@@ -2,12 +2,15 @@ import numpy as np
 import time
 
 from VelocityModel import Vector3D, create_velocity_model
-from main import angular, frequency_samples, born, time_samples, create_header, save_seismogram
+from main import angular, frequency_samples, born, time_samples, create_header, \
+    save_seismogram
+from units import Seconds, RadiansPerSecond
 
 """
-This script generates data to recreate fig. 5b) from the paper 3D seismic characterization of 
-fractures in a dipping layer using the double-beam method by Hao Hu and Yingcai Zheng. The data
-is generated using the same born modeling method as mentioned in the paper. 
+This script generates data to recreate fig. 5b) from the paper 3D seismic 
+characterization of fractures in a dipping layer using the double-beam method 
+by Hao Hu and Yingcai Zheng. The data is generated using the same born modeling 
+method as mentioned in the paper. 
 """
 
 #
@@ -22,9 +25,9 @@ num_of_receivers = 100
 #
 source_pos = Vector3D(11200., 5600., 10.)
 vm = create_velocity_model()
-omega_central = angular(30.)
-length = 4
-sample_period = 0.004
+omega_central: RadiansPerSecond = angular(30.)
+length: Seconds = 4
+sample_period: Seconds = 0.004
 # format id leftpadded with zeros
 output_filename = "receiver_{id:03d}.txt"
 f_samples = frequency_samples(length, sample_period)
@@ -37,7 +40,8 @@ def generate_seismograms_for_receivers():
         for index, (x, y) in enumerate(zip(receivers_x, receivers_y)):
             before = time.time()
             current_receiver = Vector3D(x, y, 0.)
-            seismogram = born(source_pos, current_receiver, vm, omega_central, f_samples)
+            seismogram = born(source_pos, current_receiver, vm, omega_central,
+                              f_samples)
             t_samples = time_samples(length, sample_period)
             header = create_header(current_receiver, source_pos)
             fname = output_filename.format(id=index)
