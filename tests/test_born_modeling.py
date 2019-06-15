bf06e22b1e45d1889c708f400a74e15fffbd6f25
Darius Arnold <mail@dariusarnold.de>
2019-06-15 16:32:13 +0200

    Add test for source/receiver symmetry

--- tests/test_born_modeling.py
+++ tests/test_born_modeling.py
@@ -19,3 +19,17 @@ class TestOneShotResult(unittest.TestCase):
                           omega_samples)
         expected = load_seismogram(Path("test_inputs/output_oneshot.txt"))[1]
         np.testing.assert_allclose(seismogram, expected)
+
+    def test_born_symmetry(self):
+        parser = _setup_parser()
+        with open("test_inputs/options_oneshot.txt", "r") as f:
+            args_text = f.read().replace("\n", " ")
+        args = parser.parse_args(args_text.split())
+        omega_samples = frequency_samples(args.timeseries_length, args.sample_period)
+        s1 = born(args.source_pos, args.receiver_pos, args.model, args.omega_central,
+                  omega_samples)
+        s2 = born(args.receiver_pos, args.source_pos, args.model, args.omega_central,
+                  omega_samples)
+        msg = ("Receiver to source seismogram not matching source to receiver"
+               "seismogram.")
+        np.testing.assert_allclose(s1, s2, err_msg=msg)
