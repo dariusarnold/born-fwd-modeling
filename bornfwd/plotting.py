8405dcc25575e530ed6b7c5e2a422926a2642a68
Darius Arnold <mail@dariusarnold.de>
2019-12-09 12:56:19 +0100

    Modify functions that plots shotgathers so it works for my data

--- bornfwd/plotting.py
+++ bornfwd/plotting.py
@@ -96,16 +96,19 @@ def plot_time_series(data: np.ndarray, timesteps: np.ndarray,
 
 def plot_seismogram_gather(seismograms: np.ndarray) -> None:
     """Plot all seismograms from a shot to recreate fig 5b) from Hu2018a"""
-    plt.pcolormesh((seismograms.T), cmap="RdGy", antialiased=True)
-    cb = plt.colorbar()
+    fig, ax = plt.subplots(figsize=(8,6), dpi=244)
+    plot = ax.pcolormesh(seismograms[480:,...].T, cmap="RdGy_r", antialiased=True)
+    cb = fig.colorbar(plot)
+    cb.set_label("Amplitude")
     # invert y axis so origin is in top left
-    plt.ylim(plt.ylim()[::-1])
+    ax.set_ylim(plt.ylim()[::-1])
     # ugly and overly specific way to limit the plotting to 1.5-3 secs
     # this is valid for 4 secs trace length with dt = 0.004 s
-    plt.ylim(ymin=750, ymax=375)
+    ax.set_ylim(ymin=750, ymax=375)
     # label y axis with seconds
-    plt.yticks(np.linspace(375, 750, 4), [f"{x:.2f}" for x in np.linspace(1.5, 3, 4)])
-    cb.set_label("Amplitude")
-    plt.xlabel("Trace #")
-    plt.ylabel("Time (s)")
-    plt.show()
+    ax.set_yticks(np.linspace(375, 750, 4))
+    ax.set_yticklabels([f"{x:.2f}" for x in np.linspace(1.5, 3, 4)])
+    ax.set_xlabel("Trace #")
+    ax.set_ylabel("Time (s)")
+    #plt.show()
+    plt.savefig("shot_gather.png", dpi=300)
