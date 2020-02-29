54bdec331735154d05ba7702dc6c676ba86e96f0
Darius Arnold <mail@dariusarnold.de>
2020-03-01 00:46:24 +0100

    Pass files as arguments

--- scripts/plot_geometry.py
+++ scripts/plot_geometry.py
@@ -1,16 +1,18 @@
-from bornfwd.plotting import plot_recording_geometry_3D, RecordingGeometryInfo
+from pathlib import Path
+
+from bornfwd.plotting import plot_recording_geometry_3D, plot_recording_geometry, RecordingGeometryInfo
 from bornfwd.io import read_sources, read_stations
-from marcellus_multiple_fractures import create_velocity_model
+from marcellus import create_velocity_model
 
 
-def main():
+def main(sourcefile: Path, receiverfile: Path):
     vm = create_velocity_model()
-    sources = read_sources("../sources.txt")
-    receivers = read_stations("../receivers.txt")
-    plot_recording_geometry_3D(sources, receivers, vm,
+    sources = read_sources(sourcefile)
+    receivers = read_stations(receiverfile)
+    plot_recording_geometry(sources, receivers, vm,
                                RecordingGeometryInfo(num_of_receiver_lines=13, receivers_per_line=50,
                                                      num_of_source_lines=25, sources_per_line=40))
 
 
 if __name__ == '__main__':
-    main()
+    main("/home/darius/masterarbeit/output_0degrees/sources.txt", "/home/darius/masterarbeit/output_0degrees/receivers.txt")
